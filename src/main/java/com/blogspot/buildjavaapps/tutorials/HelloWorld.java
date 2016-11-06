45165ab0f61cb159324827b980745e35a31cb94e
thefearless123 <udayaprakash123@gmail.com>
2016-11-05 19:37:09 -0500

    Initial project commit with simple hello world program

---
+++ src/main/java/com/blogspot/buildjavaapps/tutorials/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.blogspot.buildjavaapps.tutorials;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World! This is a simple java program.");
+	}
+
+}
